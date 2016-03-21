df71383ca52c891df00280a0f0a58a2c7e3693b6
Stanislav Yotov <yotov.st@gmail.com>
2016-03-21 01:33:18 +0000

    JSON parsing example

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+
+import json.Person;
 
 public class Main {
 
 	public static void main(String[] args) {
 
+		//CSV
 		ParsingCSV pCSV = new ParsingCSV();
 		try {
 			int readStatus = pCSV.readWHeader("files/data.csv");
@@ -13,7 +22,18 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	
+		
+		//JSON
+	    Gson gson = new Gson();
+	    Person person = null;
+		try {
+			person = gson.fromJson(new FileReader("files/data.json"), Person.class);
+		} catch (JsonSyntaxException | JsonIOException | FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    System.out.println(gson.toJson(person));
+	    System.out.println(person.toString());
 
 	}
 
---
+++ src/json/Person.java
@@ -0,0 +1,40 @@
+package json;
+
+import java.util.List;
+
+public class Person {
+	String firstName, lastName;
+	int age;
+	Address address;
+	List<PhoneNumber> phoneNumbers;
+
+	class Address {
+		String streetAddress, city, state;
+		int postalCode;
+
+		@Override
+		public String toString() {
+			return "\"streetAddress\": \"" + this.streetAddress + "\",\n\"city\": \"" + this.city + "\",\n\"state\": \""
+					+ this.state + "\",\n\"postalCode\": \"" + this.postalCode + "\"";
+		}
+	}
+
+	class PhoneNumber {
+		String type, number;
+
+		@Override
+		public String toString() {
+			return "\"type\": \"" + this.type + "\",\n\"number\": \"" + this.number + "\"";
+		}
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "fistName: " + firstName+
+				"lastName: " + lastName+
+				"age: " + age +
+				"address: " + address.toString()+
+				"phoneNumbers: " + phoneNumbers.toString();
+	}
+}
